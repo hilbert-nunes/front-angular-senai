a0fd231cd0451dd6e606960f2d571390959a8c3e
Hilbert Nunes <hilbert.nunes.dev@gmail.com>
2023-12-30 18:57:34 -0300

    add material login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,12 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  template: `
-    <p>
-      login works!
-    </p>
-  `,
-  styles: ``
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
